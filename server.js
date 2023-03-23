38132e48196c01939719f806ef0f5352f8eaf5f6
Julianne <juliaguas17@gmail.com>
2023-03-23 15:45:46 -0500

    edits to dependencies

--- server.js
+++ server.js
@@ -4,23 +4,11 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({
-    helpers
-});
+
+const hbs = exphbs.create({ helpers });
+
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const sess = {
-    secret: process.env.DB_SECRET,
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-        checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
-        expiration: 1000 * 60 * 30 // expires after 30 minutes
-    })
-};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -28,16 +16,15 @@ const PORT = process.env.PORT || 3001;
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use(routes);
+app.use(require('./controllers/'));
 
-sequelize.sync();
-
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-});
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  });
