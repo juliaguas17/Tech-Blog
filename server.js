1df804e694dd06406c2cd0e0d5f67805a2e8303a
Julianne <juliaguas17@gmail.com>
2023-03-23 18:21:36 -0500

    deleted node modules

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 
 const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.DB_SECRET,
+    secret: 'Super secret secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-        db: sequelize,
-        checkExpirationInterval: 1000 * 60 * 10, // will check every 10 minutes
-        expiration: 1000 * 60 * 30 // will expire after 30 minutes
+      db: sequelize
     })
-};
+  };
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -36,7 +35,6 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(routes);
-app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
